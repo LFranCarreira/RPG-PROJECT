4fdd1c147f20d9ac42b39594bd4acb3fa4613865
Franco Carreira <carreirafranco@gmail.com>
2024-03-18 18:20:13 -0300

    change some things

--- RPG Project/script.js
+++ RPG Project/script.js
@@ -16,6 +16,14 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
+const locations = [
+  {
+    name: 'town square',
+    'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
+    'button functions': [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+];
 
 //initialize buttons
 
@@ -23,6 +31,7 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location) {}
 function goTown() {
   button1.innerText = 'Go to store';
   button2.innerText = 'Go to cave';
