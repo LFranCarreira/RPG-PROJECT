b030fb754652f37340787ee2ddffed079c3c25a5
Franco Carreira <carreirafranco@gmail.com>
2024-03-18 10:14:24 -0300

    New functions

--- RPG Project/script.js
+++ RPG Project/script.js
@@ -23,14 +23,25 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function goStore() {
-  button1.onclick = buyHealth();
+function goTown() {
   button1.innerText = 'Buy 10 health (10 gold)';
-  button2.onclick = buyWeapon();
   button2.innerText = 'Buy weapon (30 gold)';
-  button3.onclick = goTown();
   button3.innerText = 'Go to town square';
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = 'You enter the store.';
 }
+function goStore() {
+  button1.innerText = 'Buy 10 health (10 gold)';
+  button2.innerText = 'Buy weapon (30 gold)';
+  button3.innerText = 'Go to town square';
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = 'You enter the store.';
+}
+
 function goCave() {
   console.log('Going to cave.');
 }
@@ -38,3 +49,7 @@ function goCave() {
 function fightDragon() {
   console.log('Fighting dragon.');
 }
+
+function buyHealth() {}
+
+function buyWeapon() {}
